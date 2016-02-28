abb0e5c338db496b698ce95bac8b7727514e85e9
Kjetil Kjeka <kjetilkjeka@gmail.com>
2016-02-28 18:04:03 +0100

    Tied together jsx and my chat_server application in a not very fine manner

---
+++ Server-erl/src/chat_server.erl
@@ -0,0 +1,25 @@
+-module(chat_server).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/0, start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start() ->
+    application:load(jsx),
+    application:start(jsx),
+    application:start(chat_server),
+    ok.
+    
+
+start(_StartType, _StartArgs) ->
+    Pid = server:start(),
+    {ok, Pid}.
+    %chat-server_sup:start_link().
+
+stop(_State) ->
+    ok.
--- Server-erl/src/server.erl
+++ Server-erl/src/server.erl
@@ -4,29 +4,34 @@
 start() ->
     Host = "localhost",
     Port = 9998,
-    spawn(fun() -> threaded_tcp_server_init(Port, Host) end),
-    ok.
+    spawn(fun() -> threaded_tcp_server_init(Port, Host) end).
 
 % We need a client handler for keeping communication with clients
 client_handler() ->
-    ok.
+    receive
+	{tcp, S, DataBinary} ->
+	    DataJson = jsx:decode(DataBinary),
+	    io:format(standard_io, "Received is ~w~n", [DataJson])
+    end,
+    client_handler().
 
 % We need soemthing waiting on the servers
 threaded_tcp_server_init(Port, Host) ->
-    io:format("Server started~n"),
+    %io:format("Server started~n"),
     {ok, IpAddress} = inet:getaddr(Host, inet),
-    io:format("ip adress resolved, it is: ~w~n", [IpAddress]),
-    {ok, ListenSocket} = gen_tcp:listen(Port, [binary, {ip, IpAddress}, {packet, 0}, {active, false}, inet]),
+    %io:format("ip adress resolved, it is: ~w~n", [IpAddress]),
+    {ok, ListenSocket} = gen_tcp:listen(Port, [binary, {ip, IpAddress}, {packet, 0}, {active, true}, inet]),
     threaded_tcp_server(ListenSocket).
 
 threaded_tcp_server(ListenSocket) -> % name is bad to be consistent with python server
-    io:format("Init thing done~n"),
+    %io:format("Init thing done~n"),
     {ok, Socket} = gen_tcp:accept(ListenSocket),
-    io:format("Socket thing done~n"),
-    spawn(fun() -> client_coordinator(Socket) end), % maybe this onliner should be improved
+    %io:format("Socket thing done~n"),
+    io:format(standard_io, "new client connected ~n", []),
+    NewClientHandler = spawn(fun() -> client_handler() end), % maybe this onliner should be improved
+    gen_tcp:controlling_process(Socket, NewClientHandler),
     threaded_tcp_server(ListenSocket).
 
 % We also need something for managing communication between client handlers
 client_coordinator(Socket) ->
-    io:format("new client connected ~n"),
     ok.
